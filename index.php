9fb724831309377f32f98fe08e1933906fffd7c9
ThibaudLafont <thibaudlafont@gmail.com>
2019-04-06 12:41:08 +0200

    Build all html

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Meta tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <!-- Title of the page -->
+    <!-- TODO : define the title in each page template -->
+    <title>Sysmod</title>
+    
+    <!-- Links -->
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png" />
+
+    <!-- Stylesheets -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header class="container">
+        <img src="/images/logo/100px/logo-sysmod.jpg">
+        <section>
+            <div class="contact">
+                <p>Appelez-nous <span>(+33) 6 77 61 25 26</span></p>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="100%" viewBox="0 0 24 24">
+                        <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/>
+                    </svg>
+                    <span>Envoyer un email</span>
+                </button>            
+            </div>            
+        </section>
+    </header>
+
+    <!-- Navigation bar -->
+    <nav>
+        <ul>
+            <li><a href="">À propos</a></a></li>
+            <li><a href="">Sysmod</a></li>
+            <li><a href="">Projets</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+
+    <!-- Top section -->
+    <section id="top-section">
+        <div class="content">
+            <h1>SYSMOD-WEB</h1>
+            <p>Envie d'un site web ? Faites nous confiance, <br/>comme bien d'autres avant vous !</p>
+            <button class='orange-classic'>Contact</button>       
+        </div>
+    </section>
+
+    <!-- Biographie -->
+    <section id="biography" class="container">
+        <!-- Header -->
+        <div class="header">
+            <div class="deco"></div>
+            <div class="content">
+                <h2>DÉVELOPPEUR INDÉPENDANT</h2>
+                <p>À 24 ans, j'ai depuis très jeune un attrait pour la programmation, le web et les nouvelles technologies</p>
+            </div>
+            <div class="deco"></div>
+        </div>
+
+        <!-- Contenu -->
+        <div class="content">
+            <!-- Photos et réseaux -->
+            <div class="aside">
+                <img src="/images/photos/450px/thibaud-lafont-coding-session.jpg" alt="Thibaud Lafont en séance de coding">
+                <ul class="social-networks">
+                    <li ><a href="https://fr.linkedin.com/in/thibaud-lafont">
+                        <svg class="linkedIn-logo" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                    </a></li>
+                    <li><a href="">
+                        <svg class="twitter-logo" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-.139 9.237c.209 4.617-3.234 9.765-9.33 9.765-1.854 0-3.579-.543-5.032-1.475 1.742.205 3.48-.278 4.86-1.359-1.437-.027-2.649-.976-3.066-2.28.515.098 1.021.069 1.482-.056-1.579-.317-2.668-1.739-2.633-3.26.442.246.949.394 1.486.411-1.461-.977-1.875-2.907-1.016-4.383 1.619 1.986 4.038 3.293 6.766 3.43-.479-2.053 1.08-4.03 3.199-4.03.943 0 1.797.398 2.395 1.037.748-.147 1.451-.42 2.086-.796-.246.767-.766 1.41-1.443 1.816.664-.08 1.297-.256 1.885-.517-.439.656-.996 1.234-1.639 1.697z"/></svg>
+                    </a></li>
+                    <li><a href="https://github.com/ThibaudLafont">
+                        <svg class="github-logo" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
+                    </a></li>
+                </ul>
+            </div>
+            <!-- Biographie -->
+            <div class="biography">
+                <p>J'ai suivi une formation de programmation web (spécialité PHP), au cours de laquelle j'ai 
+                développé des compétences en HTML, CSS, PHP, SQL et Javascript.</p> 
+                <p>J'ai su renforcer mon savoir-faire en réalisant de nombreux projets avec des interlocuteurs variés : entreprises, organismes culturels, particuliers et universitaires.</p>
+                <button class="orange-classic">ME CONTACTER</button>
+            </div>
+        </div>
+    </section>
+
+    <!-- Choisir Sysmod -->
+    <section id="choose-sysmod">
+        <div class="container">
+            <h2>CHOISIR <span>SYSMOD-WEB</span></h2>
+            <div>
+                <img src="/images/photos/450px/thibaud-lafont_adrien-matcheret.jpg" alt="Thibaud Lafont et Adrien Matcheret">
+                <div class="content">
+                    <ul>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg>
+                            <span>Applications personnalisées</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg>
+                            <span>Hébergement de votre site web</span></li>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg>
+                            <span>Respect des bonnes pratiques du web</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg>
+                            <span>Optimisation du référencement</span>
+                        </li>
+                        <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.856 10.303c.086.554.144 1.118.144 1.697 0 6.075-4.925 11-11 11s-11-4.925-11-11 4.925-11 11-11c2.347 0 4.518.741 6.304 1.993l-1.422 1.457c-1.408-.913-3.082-1.45-4.882-1.45-4.962 0-9 4.038-9 9s4.038 9 9 9c4.894 0 8.879-3.928 8.99-8.795l1.866-1.902zm-.952-8.136l-9.404 9.639-3.843-3.614-3.095 3.098 6.938 6.71 12.5-12.737-3.096-3.096z"/></svg>
+                            <span>Backend adapté à vos besoins</span>
+                        </li>
+                    </ul>
+                    <button class="orange-classic">EN SAVOIR PLUS</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Technologies -->
+    <section id="technologies" class="container">
+        <!-- Header -->
+        <div class="header">
+            <div class="deco"></div>
+            <div class="content">
+                <h2>NOTRE TRAVAIL</h2>
+                <p>PHP, Symfony, MySQL, JS, JQuery, Boostrap, etc</p>
+            </div>
+            <div class="deco"></div>
+        </div>
+
+        <!-- First section -->
+        <section id="honeycomb-display">
+            <div>
+                <p>J'ai réalisé une formation à l'issue de laquelle j'ai obtenu un diplome de développeur d'applications web, avec une spécialité PHP/Symfony.</p>
+                <p>Par la suite j'ai eu l'occasion de mettre en pratique mes connaissances en créant diverses applications, avec diverses structures (entreprises, organismes culturels, particuliers et universitaires).</p>
+            </div>
+            <img src="/images/pictograms/honeycomb-pictograms.svg" alt="Pictograms of main technologies in honeycomb">
+        </section>
+
+        <!-- Pictograms section -->
+        <section id="legended-infos">
+            <div>
+                <div class="part">
+                    <img src="/images/pictograms/100px/informatique-inverse.png" alt="Computer pictogram">
+                    <p>Nous développons tout type d'application avec des technologies, puissantes, fiables et 
+                    éprouvées</p>
+                </div>
+                <div class="part">
+                    <img src="/images/pictograms/100px/globe.png" alt="Global pictogram">
+                    <p>Offrez-vous une visibilité en ligne, et profitez d'un site web adapté à vos
+                    besoins et à ceux de vos clients</p>
+                </div>
+            </div>
+            <div>
+                <div class="part">
+                    <img src="/images/pictograms/100px/code-inverse.png" alt="Coding pictogram">
+                    <p>Profitez de l'expertise et des conseils d'un développeur confirmé, qui a de 
+                    nombreux projets à son actif</p>
+                </div>
+                <div class="part">
+                    <img src="/images/pictograms/100px/server.png" alt="Server pictogram">
+                    <p>Nous pouvons metre en en place une solution d'hébergement adaptée et performante 
+                    pour votre site web</p>
+                </div>
+            </div>
+        </section>
+
+        <!-- Contact button -->
+        <div>
+            <button class="orange-classic">CONTACT</button>
+        </div>
+    </section>
+
+    <!-- Projects -->
+    <section id="projects">
+        <div class="container">
+            <h2>QUELQUES <span>PROJETS</span></h2>
+            <div><img src="/images/logo/150px/logo-universite-tours.jpg" alt="Logo université de Tours"></div>
+            <div><img src="/images/logo/150px/logo-fondation-du-doute.jpg" alt="Logo Fondation Du Doute"></div>
+            <div><img src="/images/logo/150px/logo-famous-ultimate-crash-karaoke.jpg" alt="Logo Famous Ultimate Krash Karaoké"></div>
+            <div><img src="/images/logo/150px/logo-bertrand-malfray.jpg" alt="Logo Bertrand Malfray"></div>
+        </div>
+    </section>
+
+    <!-- Contact -->
+    <section id="contact" class="container">
+        <!-- Header -->
+        <div class="header">
+            <div class="deco"></div>
+            <div class="content">
+                <h2>NOUS CONTACTER</h2>
+                <p>Besoin d'informations ?</p>
+            </div>
+            <div class="deco"></div>
+        </div>
+        
+        <section class="content">
+            <form>
+                <input type="text" name="full-name" id="full-name" placeholder="Nom complet">
+                <input type="email" name="email" id="email" placeholder="Email">
+                <textarea placeholder="Votre message" rows="10"></textarea>
+                <button type="submit" class="orange-classic">ENVOYER</button>
+            </form>
+
+            <div>
+                <div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>
+                    </span>
+                    <span>
+                        <h4>LOCALISATION</h4>
+                        <p>Blois, Centre Val de Loire</p>
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 24 24"><path d="M20.89 23.654c-7.367 3.367-18.802-18.86-11.601-22.615l2.107-1.039 3.492 6.817-2.083 1.026c-2.189 1.174 2.37 10.08 4.609 8.994.091-.041 2.057-1.007 2.064-1.011l3.522 6.795c-.008.004-1.989.978-2.11 1.033zm-9.438-2.264c-1.476 1.072-3.506 1.17-4.124.106-.47-.809-.311-1.728-.127-2.793.201-1.161.429-2.478-.295-3.71-1.219-2.076-3.897-1.983-5.906-.67l.956 1.463c.829-.542 1.784-.775 2.493-.609 1.653.388 1.151 2.526 1.03 3.229-.212 1.223-.45 2.61.337 3.968 1.243 2.143 4.579 2.076 6.836.316-.412-.407-.811-.843-1.2-1.3z"/></svg>
+                    </span>
+                    <span>
+                        <h4>TÉLÉPHONE</h4>
+                        <p>(+33) 6 77 61 25 26</p>
+                    </span>
+                </div>
+            </div>
+        </section>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <section class="container">
+            <img src="/images/logo/150px/logo-sysmod.png">
+        </section>
+        <p>
+            Copyright © <?php echo date('Y'); ?> Sysmod - Tous droits réservés
+        </p>
+    </footer>
+</body>
+
+</html>
