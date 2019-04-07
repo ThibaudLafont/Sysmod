81435b2aac7e56dc19b648769a476bb8a22477cd
ThibaudLafont <thibaudlafont@gmail.com>
2019-04-07 15:26:42 +0200

    Build the two templates for confirm and contact

--- backend/sendMail.php
+++ backend/sendMail.php
@@ -5,7 +5,28 @@ use PHPMailer\PHPMailer\Exception;
 // Load Composer's autoloader
 require '../vendor/autoload.php';
 require 'mailFunction.php';
+require 'templates/templates.php';
 
-// Instantiation and passing `true` enables exceptions
+
+$content = 'lorem teauygauza zaiudhiza ud';
+
+
+// Mail à admin
+$mail2 = new PHPMailer(true);
+sendMail(
+	$mail2,
+	'contact@sysmod.fr',
+	'Sysmod',
+	'Demande de contact - sysmod.fr',
+	generateAdminHtmlContent('Thibaud Lafont', 'thibaudlafont@gmail.com','06 77 61 25 26', $content),
+	generateAdminRawContent('Thibaud Lafont', 'thibaudlafont@gmail.com','06 77 61 25 26', $content));
+
+// Confirmation d'envoi
 $mail = new PHPMailer(true);
-sendMail($mail, 'thibaudlafont@gmail.com', 'Thibaud Lafont', 'Lalala');
+sendMail(
+	$mail, 
+	'thibaudlafont@gmail.com', 
+	'Thibaud Lafont', 
+	'Merci pour votre mail!', 
+	generateClientHtmlContent($content), 
+	generateClientRawContent($content));
